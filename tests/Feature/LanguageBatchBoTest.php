e3beb7b3baf6aca02d41fe1cc43b99a378c3b098
Istvan Drahos <drahos.istvan@gmail.com>
2017-10-24 12:04:21 +0200

    Updating phpunit version (5.7 is compatible with PHP 5.6 - we can test different PHP versions) Adding phpunit xml file Adding a simple test, to test the PHPUnit setup

---
+++ tests/Feature/LanguageBatchBoTest.php
@@ -0,0 +1,10 @@
+<?php
+
+class LanguageBatchBoTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function it_tests_phpunit_setup()
+    {
+        $this->assertTrue(true, true);
+    }
+}
