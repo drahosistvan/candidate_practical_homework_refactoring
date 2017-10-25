497c8b74f73a4f1ebc45137578e7d8290df1e89f
Istvan Drahos <drahos.istvan@gmail.com>
2017-10-25 12:06:06 +0200

    refactor filecacher to handle directory creaton

--- src/Services/Cache/FileCache.php
+++ src/Services/Cache/FileCache.php
@@ -11,6 +11,9 @@ use Psr\Log\LoggerInterface;
 class FileCache implements CacheDriver
 {
     private $logger;
+    private $folder;
+    private $filename;
+
     public function __construct(LoggerInterface $logger)
     {
         $this->logger = $logger;
@@ -18,22 +21,40 @@ class FileCache implements CacheDriver
 
     public function set(ApplicationLanguage $language)
     {
-        if (file_put_contents($this->getPathForApplicationLanguage($language), $language->getCacheContent()) === false) {
-            $this->logger->error('Cannot write to file', ['path' => $this->getPathForApplicationLanguage($language)]);
+        $this->folder = $this->getPathForApplicationLanguage($language);
+        $this->filename = $this->getFilenameForApplicationLanguage($language);
+        if (!is_dir($this->folder)) {
+            mkdir($this->folder);
+        }
+        if (file_put_contents($this->folder.$this->filename, $language->getCacheContent()) === false) {
+            $this->logger->error('Cannot write to file', ['path' => $this->folder.$this->filename]);
             throw new CacheCreationException('Cannot write the file');
         }
 
-        $this->logger->info('Language file created successfully', ['path' => $this->getPathForApplicationLanguage($language)]);
+        $this->logger->info('Language file created successfully', ['path' => $this->folder.$this->filename]);
         return true;
     }
 
     protected function getPathForApplicationLanguage(ApplicationLanguage $language) {
         switch ($language->type) {
             case 'applet':
-                return Config::get('system.paths.root') . '/cache/flash/lang_' . $language->language . '.xml';
+                return Config::get('system.paths.root') . '/cache/flash/';
                 break;
             case 'standard':
-                return Config::get('system.paths.root') . '/cache/' . $language->application . '/'. $language->language . '.php';
+                return Config::get('system.paths.root') . '/cache/'.$language->application . '/';
+                break;
+            default:
+                return;
+        }
+    }
+
+    protected function getFilenameForApplicationLanguage(ApplicationLanguage $language) {
+        switch ($language->type) {
+            case 'applet':
+                return 'lang_' . $language->language . '.xml';
+                break;
+            case 'standard':
+                return $language->language . '.php';
                 break;
             default:
                 return;
