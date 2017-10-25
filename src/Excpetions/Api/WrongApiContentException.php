d8e22b3138a38b1042bfa27f3b0c9d1f7512ee52
Istvan Drahos <drahos.istvan@gmail.com>
2017-10-25 12:04:52 +0200

    delete wrong folder

--- src/Excpetions/Api/WrongApiContentException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Language\Exceptions\Api;
-
-class WrongApiContentException extends \Exception
-{
-}
--- src/Excpetions/Api/WrongApiResponseException.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Language\Exceptions\Api;
-
-class WrongApiResponseException extends \Exception
-{
-}
